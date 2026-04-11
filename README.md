61f95d8b2d70192a95ea0618d6fb4ec85f2a2457
left-alert-chaos <left-alert-chaos@gmail.com>
2026-04-10 22:03:39 -0400

    Add disclaimer to README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Sophisticate
-Sophisticate is a chess bot that is definitely not sophisticated.
+Sophisticate is a chess bot that is decidedly not sophisticated.
+
+It's currently not nearly usable yet, and in active development. It's little more than around 300 lines of Python that can't calculate all possible moves or filter illegal moves. It's really going places, though!
--- testing/physics_test.py
+++ testing/physics_test.py
@@ -5,11 +5,6 @@ from src import physical
 
 game = physical.board.Board()
 
-print(game["e"][1].piece)
-pprint(physical.movement.potential_moves(game["e"][1].piece, game))
-pprint(physical.movement.potential_moves(game["e"][6].piece, game))
-pprint(physical.movement.potential_moves(game["a"][0].piece, game))
-
 #add a rogue rook
 game["e"][3].piece = physical.board.Piece(physical.board.PieceType.ROOK, physical.board.PieceColor.WHITE, game["e"][3])
 pprint(physical.movement.potential_moves(game["e"][3].piece, game))
