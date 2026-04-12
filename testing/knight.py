d3f552fa67a7c3dd0db37f7df25a0bbeb5fdfdc9
left-alert-chaos <left-alert-chaos@gmail.com>
2026-04-12 08:38:01 -0400

    find all knight moves, but don't check for legality

---
+++ testing/knight.py
@@ -0,0 +1,9 @@
+import testlib
+from src.physical import board, movement
+
+game = board.Board()
+print("White's b knight's legal opening moves (should be a2 and c2):")
+print(movement.potential_moves(game["b"][0].piece, game))
+print("Black's b knight's legal opening moves (should be a5 and c5):")
+print(movement.potential_moves(game["b"][7].piece, game))
+
