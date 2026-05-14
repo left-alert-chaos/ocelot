db77dd8ca5f8b10bb26d5d053cc5d579fdbdbf6f
left-alert-chaos <left-alert-chaos@gmail.com>
2026-05-13 20:21:43 -0400

    Add layout of physical module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod physical;
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/physical/mod.rs
@@ -0,0 +1,7 @@
+//! All physical representation handling.
+//!
+//! Pieces, types, colors, and the board are handled in the `board` module.
+//! Movement, castling, and move generation are handled in the `movement` module.
+
+pub mod board;
+pub mod movement;
