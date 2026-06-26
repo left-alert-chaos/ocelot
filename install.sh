83df744c581325a2f101346a4110a79d3bca9ca1
left-alert-chaos <left-alert-chaos@gmail.com>
2026-06-26 18:16:58 -0400

    Stop making config directory in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-echo "This will create an engines directory in your home directory and a directory in ~/.local/share/ocelot-chess"
+echo "This will create an engines directory in your home directory."
 mkdir -p ~/engines/
 cargo build --release
 mv ./target/release/ocelot-chess ~/engines/
-
-mkdir -p ~/.local/share/ocelot-chess
-touch ~/.local/share/ocelot-chess/uci-log
